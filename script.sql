bf658d2f1fb7b599669a00840e7c4f4a6b656aa1
Sachith Rangana1 <sachith2546@gmail.com>
2024-08-08 17:00:21 +0530

    Add table

--- script.sql
+++ script.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE dep12_join_example_2;
 
 USER dep12_join_example_2;
+
+CREATE TABLE customer(
+                         id VARCHAR(8) PRIMARY KEY ,
+                         name VARCHAR(100) NOT NULL ,
+                         address VARCHAR(500) NOT NULL
+);
