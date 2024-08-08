c8e2b7008c5045c4ac8fd0b66fcd02ac0835a2a6
ayeshmantha <ravindu.ayesh123@gmail.com>
2024-08-08 17:23:48 +0530

    Add order relation

--- script.sql
+++ script.sql
@@ -15,3 +15,13 @@ CREATE TABLE "user"
     name     VARCHAR(200) NOT NULL,
     password VARCHAR(300) NOT NULL
 );
+
+CREATE TABLE "order"
+(
+    id VARCHAR(10) PRIMARY KEY ,
+    date DATE,
+    customer_id VARCHAR(10) NOT NULL,
+    username VARCHAR(100) NOT NULL ,
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer,
+    CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES "user"
+);
