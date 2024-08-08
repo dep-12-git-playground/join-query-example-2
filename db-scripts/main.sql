816f7140e9364c73391bf9be6d59097d6564d58e
hiron <hironkanishka97@gmail.com>
2024-08-08 17:42:25 +0530

    Insert order_item values

--- db-scripts/main.sql
+++ db-scripts/main.sql
@@ -14,3 +14,14 @@ CREATE TABLE item
     stock       INT           NOT NULL,
     price       DECIMAL(9, 2) NOT NULL
 );
+
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('300.00','2','BAR00001','OD001');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('500.00','3','BAR00002','OD002');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('150.00','1','BAR00003','OD003');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('200.00','4','BAR00004','OD004');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('100.00','3','BAR00005','OD005');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('150.00','5','BAR00003','OD006');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('75.00','1','BAR00006','OD007');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('200.00','2','BAR00007','OD008');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('500.00','3','BAR00002','OD009');
+INSERT INTO order_item (price, qty, bar_code, order_id) VALUES ('140.00','5','BAR00008','OD010');
