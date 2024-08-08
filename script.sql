cc984d206e00088f42ee7b8157d5d22b5d522aae
sanduni <sanduniakn@gmail.com>
2024-08-08 17:00:46 +0530

    Add order_item table

--- script.sql
+++ script.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE dep12_join_example_2;
 
 USER dep12_join_example_2;
+
+CREATE TABLE order_item(
+                           item_code VARCHAR(50) NOT NULL ,
+                           order_id VARCHAR(10) NOT NULL,
+                           price DECIMAL(9, 2) NOT NULL ,
+                           qty INT NOT NULL ,
+                           CONSTRAINT pk_order_item PRIMARY KEY (item_code, order_id),
+                           CONSTRAINT fk_item FOREIGN KEY (item_code) REFERENCES item(bar_code),
+                           CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES 'order'(id)
+);
