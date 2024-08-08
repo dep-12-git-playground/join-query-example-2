01f92d8d09df86d8eed95896342ac6a6c1cbb78c
Menith <menith.somarathne@gmail.com>
2024-08-08 17:15:37 +0530

    updated

--- script.sql
+++ script.sql
@@ -8,3 +8,12 @@ CREATE TABLE customer
     id   VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE order(
+                      id VARCHAR(10) Primary Key,
+                      date DATE NOT NULL,
+                      customer_id VARCHAR(10) NOT NULL,
+                      username VARCHAR(100) NOT NULL ,
+                      CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
+                      CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
+);
