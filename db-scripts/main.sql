9b06e15c6b6bf81e31bf29f3adb73d114e835bdf
shanuka1198 <shanukaimantha0@gmail.com>
2024-08-08 17:49:03 +0530

    Order relation1

--- db-scripts/main.sql
+++ db-scripts/main.sql
@@ -14,3 +14,18 @@ CREATE TABLE item
     stock       INT           NOT NULL,
     price       DECIMAL(9, 2) NOT NULL
 );
+
+CREATE TABLE "order"(
+                        order_id VARCHAR(10)PRIMARY KEY,
+                        date DATE NOT NULL,
+                        customer_id VARCHAR(10)NOT NULL,
+                        user_name VARCHAR(100)NOT NULL,
+                        CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+                        CONSTRAINT fk_user FOREIGN KEY (user_name) REFERENCES "user"(username)
+
+                            INSERT INTO "order"(order_id, "date", customer_id, user_name) VALUES
+    ('OD001','2024-08-07','C001','U001'),
+    ('OD002','2024-08-07','C002','U004'),
+    ('OD003','2024-08-07','C003','U005'),
+    ('OD004','2024-08-07','C004','U002'),
+    ('OD005','2024-08-07','C005','U003');
