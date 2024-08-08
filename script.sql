c9a9ce78856e27073c69d5565a8bcc37570fba0b
Vimukthi Chandrasekera <vimukthi1157@gmail.com>
2024-08-08 16:50:04 +0530

    Add order table

--- script.sql
+++ script.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE dep12_join_example_2;
 
 USER dep12_join_example_2;
+
+CREATE TABLE order(
+    id VARCHAR(10) Primary Key,
+    date DATE NOT NULL,
+    customer_id VARCHAR(10) NOT NULL,
+    username VARCHAR(100) NOT NULL ,
+    CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
+    CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
+);
