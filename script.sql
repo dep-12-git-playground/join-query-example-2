c200a13cd1b751cfd0409958447d84f869c82492
Menith <menith.somarathne@gmail.com>
2024-08-08 17:23:25 +0530

    created item table

--- script.sql
+++ script.sql
@@ -9,18 +9,9 @@ CREATE TABLE customer
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE order(
-                      id VARCHAR(10) Primary Key,
-                      date DATE NOT NULL,
-                      customer_id VARCHAR(10) NOT NULL,
-                      username VARCHAR(100) NOT NULL ,
-                      CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
-                      CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
-                          CREATE TABLE order(
-                          id VARCHAR(10) Primary Key,
-                          date DATE NOT NULL,
-                          customer_id VARCHAR(10) NOT NULL,
-                          username VARCHAR(100) NOT NULL ,
-                          CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
-                          CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
-                          );
+CREATE TABLE item(
+                     bar_code VARCHAR(50) PRIMARY KEY,
+                     description VARCHAR(300) NOT NULL,
+                     stock INT NOT NULL,
+                     price DECIMAL(10,2)
+);
