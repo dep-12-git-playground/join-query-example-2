8d688fbed4eca875f93c5277cfb969a5d8a86826
ayeshmantha <ravindu.ayesh123@gmail.com>
2024-08-08 17:41:57 +0530

    Add order-item relation

--- db-scripts/main.sql
+++ db-scripts/main.sql
@@ -1,13 +1,10 @@
-CREATE
-DATABASE dep12_join_example_2;
+CREATE DATABASE dep12_join_example_2;
 
-USE
-dep12_join_example_2;
+USE dep12_join_example_2;
 
-CREATE TABLE customer
-(
-    id   VARCHAR(10) PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+CREATE TABLE customer(
+  id VARCHAR(10) PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE item
@@ -34,3 +31,13 @@ CREATE TABLE "order"(
                           CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
 );
 
+
+CREATE TABLE order_item(
+                           price DECIMAL NOT NULL ,
+                           qty INT NOT NULL ,
+                           bar_code VARCHAR(50) NOT NULL ,
+                           order_id VARCHAR(10) NOT NULL ,
+                           CONSTRAINT fk_bar_code FOREIGN KEY (bar_code) REFERENCES item,
+                           CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES "order",
+                           CONSTRAINT pk_order_item PRIMARY KEY (bar_code,order_id)
+);
