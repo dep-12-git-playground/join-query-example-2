7fa6047d09ce401cac82123b951464e011cb8825
Waruna Wijekoon <warunawijekoon01@gmail.com>
2024-08-08 16:50:03 +0530

    add item relation

--- script.sql
+++ script.sql
@@ -1,3 +1,8 @@
 CREATE DATABASE dep12_join_example_2;
 
 USER dep12_join_example_2;
+CREATE TABLE item (
+                      bar_code VARCHAR(50) PRIMARY KEY,
+                      description VARCHAR(150) NOT NULL ,
+                      price DECIMAL(8,2) NOT NULL,
+                      stock INT NOT NULL);
