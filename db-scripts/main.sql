34065a7dea71948e70c8db47a4bd138bef099afb
LahiruHarshana <lharshana2002@gmail.com>
2024-08-08 17:42:53 +0530

    add order

--- db-scripts/main.sql
+++ db-scripts/main.sql
@@ -14,3 +14,12 @@ CREATE TABLE item
     stock       INT           NOT NULL,
     price       DECIMAL(9, 2) NOT NULL
 );
+
+CREATE TABLE `order`(
+                          id VARCHAR(10) Primary Key,
+                          date DATE NOT NULL,
+                          customer_id VARCHAR(10) NOT NULL,
+                          username VARCHAR(100) NOT NULL ,
+                          CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
+                          CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
+);
