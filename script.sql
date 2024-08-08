9813701e9826caaa1ec3ed1d446eb838444b5626
Yasith Ishara Perera <yasithishara@gmail.com>
2024-08-08 16:52:54 +0530

    Created Order Relation

---
+++ script.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE dep12_join_example_2;
+
+USER dep12_join_example_2;
+
+CREATE TABLE "order"
+(
+    id          VARCHAR(10) PRIMARY KEY,
+    customer_id VARCHAR(10) REFERENCES customer (id),
+    username    VARCHAR(100) REFERENCES user (username),
+    date        DATE NOT NULL
+);
+
+CREATE TABLE customer
+(
+    id   VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE "user"
+(
+    username VARCHAR(100) PRIMARY KEY,
+    name     VARCHAR(200) NOT NULL,
+    password VARCHAR(300) NOT NULL
+);
