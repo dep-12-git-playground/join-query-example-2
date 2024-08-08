efee10242d9c8da9d101a649252618a9a65b63ee
Menith <menith.somarathne@gmail.com>
2024-08-08 17:22:38 +0530

    initial commit

--- script.sql
+++ script.sql
@@ -16,4 +16,11 @@ CREATE TABLE order(
                       username VARCHAR(100) NOT NULL ,
                       CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
                       CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
-);
+                          CREATE TABLE order(
+                          id VARCHAR(10) Primary Key,
+                          date DATE NOT NULL,
+                          customer_id VARCHAR(10) NOT NULL,
+                          username VARCHAR(100) NOT NULL ,
+                          CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
+                          CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES user(username)
+                          );
