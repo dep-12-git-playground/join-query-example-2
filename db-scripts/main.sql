2baec00c0395c850fcfa2654b19da42c8ab95093
ayeshmantha <ravindu.ayesh123@gmail.com>
2024-08-08 18:03:59 +0530

    Add customer records

--- db-scripts/main.sql
+++ db-scripts/main.sql
@@ -24,3 +24,14 @@ CREATE TABLE `user`
     name     VARCHAR(100) NOT NULL,
     password VARCHAR(300) NOT NULL
 );
+
+INSERT INTO customer (id, name) VALUES ('C001', 'Kasun Sampath');
+INSERT INTO customer (id, name) VALUES ('C002', 'Nuwan Praddep');
+INSERT INTO customer (id, name) VALUES ('C003', 'Ruwan Ramidu');
+INSERT INTO customer (id, name) VALUES ('C004', 'Pasan Chamara');
+INSERT INTO customer (id, name) VALUES ('C005', 'Buddika Silva');
+INSERT INTO customer (id, name) VALUES ('C006', 'Chaminda Vass');
+INSERT INTO customer (id, name) VALUES ('C007', 'Bathiya Santhush');
+INSERT INTO customer (id, name) VALUES ('C008', 'Chamara Silva');
+INSERT INTO customer (id, name) VALUES ('C009', 'Ajantha Mendis');
+INSERT INTO customer (id, name) VALUES ('C010', 'Sura Boy');
