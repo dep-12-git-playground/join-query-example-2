bb43b80b7fb71801efeabd3a09309635ffead511
Yasith Ishara Perera <yasithishara@gmail.com>
2024-08-08 16:52:54 +0530

    Created Order Relation

--- script.sql
+++ script.sql
@@ -2,23 +2,16 @@ CREATE DATABASE dep12_join_example_2;
 
 USER dep12_join_example_2;
 
-CREATE TABLE "order"
-(
-    id          VARCHAR(10) PRIMARY KEY,
-    customer_id VARCHAR(10) REFERENCES customer (id),
-    username    VARCHAR(100) REFERENCES user (username),
-    date        DATE NOT NULL
-);
-
 CREATE TABLE customer
 (
     id   VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE "user"
+CREATE TABLE "order"
 (
-    username VARCHAR(100) PRIMARY KEY,
-    name     VARCHAR(200) NOT NULL,
-    password VARCHAR(300) NOT NULL
+    id          VARCHAR(10) PRIMARY KEY,
+    customer_id VARCHAR(10) REFERENCES customer (id),
+    username    VARCHAR(100) REFERENCES user (username),
+    date        DATE NOT NULL
 );
