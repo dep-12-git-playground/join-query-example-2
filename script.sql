fed96ca3c757eac6317a58bc0be6c9ef43423ec1
ayeshmantha <ravindu.ayesh123@gmail.com>
2024-08-08 17:23:48 +0530

    Add order relation

---
+++ script.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE dep12_join_example_2;
+
+USER dep12_join_example_2;
+
+
+CREATE TABLE customer
+(
+    id   VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE "user"
+(
+    username VARCHAR(100) PRIMARY KEY,
+    name     VARCHAR(200) NOT NULL,
+    password VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE "order"
+(
+    id VARCHAR(10) PRIMARY KEY ,
+    date DATE,
+    customer_id VARCHAR(10) NOT NULL,
+    username VARCHAR(100) NOT NULL ,
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer,
+    CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES "user"
+);
